b2f300b36596f6453e141e544b4508399dcd3c7b
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-08 02:42:53 +0900

    [Feat] 공통 응답 클래스 생성

---
+++ src/main/java/com/wemo/backend/global/response/SuccessResponse.java
@@ -0,0 +1,33 @@
+package com.wemo.backend.global.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SuccessResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+
+    private static <T> SuccessResponse<T> success(T data, String message) {
+
+        return SuccessResponse.<T>builder()
+                .success(true)
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+    public static <T> SuccessResponse<T> successWithData(T data) {
+
+        return success(data, "OK");
+    }
+
+    public static <T> SuccessResponse<T> successWithNoData(String message) {
+
+        return success(null, message);
+    }
+
+}
