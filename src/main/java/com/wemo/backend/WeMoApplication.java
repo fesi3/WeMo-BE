df3cb6d2bafed20565ee1e864b9d48b8e837186f
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-07 17:58:31 +0900

    Initial commit

---
+++ src/main/java/com/wemo/backend/WeMoApplication.java
@@ -0,0 +1,13 @@
+package com.wemo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeMoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeMoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wemo/backend/WeMoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wemo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeMoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
