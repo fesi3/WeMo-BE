667532aff15f8fc2790114fa08fa29c3c3b9e7f1
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-08 18:16:04 +0900

    [Feat] 요청 객체 생성

--- src/main/java/com/wemo/backend/domain/user/dto/UserCreateRequest.java
+++ src/main/java/com/wemo/backend/domain/user/dto/UserCreateRequest.java
@@ -1,4 +1,43 @@
 package com.wemo.backend.domain.user.dto;
 
+import com.wemo.backend.domain.user.entity.User;
+import com.wemo.backend.domain.user.service.validation.ValidPassword;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserCreateRequest {
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    private String email;
+
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    @Size(min = 2, max = 20, message = "닉네임은 최소 2자, 최대 20자여야 합니다.")
+    private String nickname;
+
+    @NotBlank(message = "회사명은 필수 입력 값입니다.")
+    private String companyName;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @ValidPassword
+    private String password;
+
+    @NotBlank(message = "비밀번호 확인은 필수 입력 값입니다.")
+    @ValidPassword
+    private String passwordCheck;
+
+    public User createUser() {
+        return User.builder()
+                .email(email)
+                .nickname(nickname)
+                .companyName(companyName)
+                .password(password)
+                .build();
+    }
+
 }
