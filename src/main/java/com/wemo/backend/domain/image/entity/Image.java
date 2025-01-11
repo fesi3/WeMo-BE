cd7066376c5666522030e8978d91b7261ef196b8
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-11 11:51:46 +0900

    [Feat] 이미지 도메인 생성

--- src/main/java/com/wemo/backend/domain/image/entity/Image.java
+++ src/main/java/com/wemo/backend/domain/image/entity/Image.java
@@ -1,4 +1,58 @@
 package com.wemo.backend.domain.image.entity;
 
-public class Image {
+import com.wemo.backend.domain.user.entity.User;
+import com.wemo.backend.global.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "TB_IMAGE")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id", nullable = false)
+    @Comment("이미지 id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // 이미지와 사용자 관계 설정 (다대일)
+    @JoinColumn(name = "user_id", nullable = false)  // user_id 외래키 설정
+    @Comment("유저 id")
+    private User user;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "entity_type", nullable = false)
+    @Comment("엔티티 타입")
+    private EntityType entityType;
+
+    @Column(name = "entity_id", nullable = false)
+    @Comment("엔티티 id")
+    private Long entityId;
+
+    @Column(name = "file_url", nullable = false)
+    @Comment("이미지 url")
+    private String fileUrl;
+
+    public enum EntityType {
+
+        PROFILE("유저 프로필"),
+        MEETING("모임"),
+        PLAN("일정"),
+        REVIEW("후기");
+
+        private String entityType;
+
+        EntityType(String entityType) {
+            this.entityType = entityType;
+        }
+    }
+
 }
