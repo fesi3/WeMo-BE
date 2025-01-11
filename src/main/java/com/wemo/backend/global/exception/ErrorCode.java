7661cee9ad8d86e2aa612b1bec6794a63a757ea6
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-11 11:58:51 +0900

    [Feat] 모임 생성 예외 처리

--- src/main/java/com/wemo/backend/global/exception/ErrorCode.java
+++ src/main/java/com/wemo/backend/global/exception/ErrorCode.java
@@ -7,12 +7,15 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // user
-    ILLEGAL_EMAIL_DUPLICATION(HttpStatus.CONFLICT, "사용 중인 이메일 입니다."),
+    ILLEGAL_EMAIL_DUPLICATION(HttpStatus.CONFLICT, "이미 사용 중인 이메일 입니다."),
     ILLEGAL_PASSWORD_NOT_VALID(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     ILLEGAL_USER_NOT_EXIST(HttpStatus.BAD_REQUEST, "가입되지 않은 아이디입니다."),
 
     // token
-    ILLEGAL_REFRESH_TOKEN_NOT_VALID(HttpStatus.BAD_REQUEST, "유효하지 않은 refreshToken 입니다.");
+    ILLEGAL_REFRESH_TOKEN_NOT_VALID(HttpStatus.BAD_REQUEST, "유효하지 않은 refreshToken 입니다."),
+
+    // category
+    ILLEGAL_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 카테고리입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
