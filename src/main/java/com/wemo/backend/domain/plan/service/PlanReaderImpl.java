00db5578768cae7812b24c773b7033e66dbc8e79
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-12 00:07:29 +0900

    [Feat] 일정 존재 여부 유효성 검사

---
+++ src/main/java/com/wemo/backend/domain/plan/service/PlanReaderImpl.java
@@ -0,0 +1,25 @@
+package com.wemo.backend.domain.plan.service;
+
+import com.wemo.backend.domain.plan.entity.Plan;
+import com.wemo.backend.domain.plan.repository.PlanRepository;
+import com.wemo.backend.global.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import static com.wemo.backend.global.exception.ErrorCode.ILLEGAL_PLAN_NOT_FOUND;
+
+@Component
+@RequiredArgsConstructor
+public class PlanReaderImpl implements PlanReader {
+
+    private final PlanRepository planRepository;
+
+    @Override
+    public Plan getPlan(Long planId) {
+
+        return planRepository.findById(planId).orElseThrow(
+                () -> new CustomException(ILLEGAL_PLAN_NOT_FOUND)
+        );
+    }
+
+}
