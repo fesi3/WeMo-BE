e4fffc08265ef504a81325270ad0b0fc6966d719
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-11 17:52:09 +0900

    [Feat] 일정 생성 예외 처리

--- src/main/java/com/wemo/backend/global/exception/ErrorCode.java
+++ src/main/java/com/wemo/backend/global/exception/ErrorCode.java
@@ -15,7 +15,14 @@ public enum ErrorCode {
     ILLEGAL_REFRESH_TOKEN_NOT_VALID(HttpStatus.BAD_REQUEST, "유효하지 않은 refreshToken 입니다."),
 
     // category
-    ILLEGAL_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 카테고리입니다.");
+    ILLEGAL_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 카테고리입니다."),
+
+    // meeting
+    ILLEGAL_MEETING_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 모임입니다."),
+
+    // plan
+    ILLEGAL_PLAN_NOT_GRANTED(HttpStatus.BAD_REQUEST, "모임장만 일정을 생성할 수 있습니다."),
+    ILLEGAL_ADDRESS_NOT_VALID(HttpStatus.BAD_REQUEST, "주소 값이 유효하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
