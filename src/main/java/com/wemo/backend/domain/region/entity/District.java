196496aa696d229d0fb87cef04ab9243c553e8b1
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-11 17:48:19 +0900

    [Feat] 지역 관련 테이블 생성

--- src/main/java/com/wemo/backend/domain/region/entity/District.java
+++ src/main/java/com/wemo/backend/domain/region/entity/District.java
@@ -1,4 +1,35 @@
 package com.wemo.backend.domain.region.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "TB_DISTRICT")
+@Getter
+@NoArgsConstructor
 public class District {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "district_id", nullable = false)
+    @Comment("군/구 id")
+    private Long id;
+
+    @Column(name = "district_name", nullable = false)
+    @Comment("군/구 이름")
+    private String districtName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "province_id")
+    @Comment("시/도 id")
+    private Province province;
+
+    public District(String districtName, Province province) {
+
+        this.districtName = districtName;
+        this.province = province;
+    }
+
 }
--- src/main/java/com/wemo/backend/domain/region/entity/Province.java
+++ src/main/java/com/wemo/backend/domain/region/entity/Province.java
@@ -1,4 +1,29 @@
 package com.wemo.backend.domain.region.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "TB_PROVINCE")
+@Getter
+@NoArgsConstructor
 public class Province {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "province_id", nullable = false)
+    @Comment("시/도 id")
+    private Long id;
+
+    @Column(name = "province_name", nullable = false, unique = true)
+    @Comment("시/도 이름")
+    private String provinceName;
+
+    public Province(String provinceName) {
+        this.provinceName = provinceName;
+
+    }
+
 }
