c2479ab8d5584a1d308bf9cb3ae4d0b7948c15ed
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-12 00:05:28 +0900

    [Feat] 일정 참여 도메인 생성

---
+++ src/main/java/com/wemo/backend/domain/plan/entity/Attendance.java
@@ -0,0 +1,34 @@
+package com.wemo.backend.domain.plan.entity;
+
+import com.wemo.backend.domain.user.entity.User;
+import com.wemo.backend.global.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "TB_ATTENDANE")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Attendance extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "attendance_id", nullable = false)
+    @Comment("참석 id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @Comment("유저 id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "plan_id")
+    @Comment("일정 id")
+    private Plan plan;
+
+}
