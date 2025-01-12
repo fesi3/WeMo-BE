a086eb768559a296b2ead13cca16bdcd41a24637
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-12 12:47:56 +0900

    [Feat] 모임에 속한 모든 일정 조회 기능 구현

--- src/main/java/com/wemo/backend/domain/plan/repository/PlanRepository.java
+++ src/main/java/com/wemo/backend/domain/plan/repository/PlanRepository.java
@@ -1,7 +1,12 @@
 package com.wemo.backend.domain.plan.repository;
 
+import com.wemo.backend.domain.meeting.entity.Meeting;
 import com.wemo.backend.domain.plan.entity.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlanRepository extends JpaRepository<Plan, Long> {
+
+    List<Plan> findAllByMeeting(Meeting meeting);
 }
--- src/main/java/com/wemo/backend/domain/plan/service/PlanReader.java
+++ src/main/java/com/wemo/backend/domain/plan/service/PlanReader.java
@@ -1,12 +1,20 @@
 package com.wemo.backend.domain.plan.service;
 
+import com.wemo.backend.domain.meeting.entity.Meeting;
+import com.wemo.backend.domain.plan.entity.Attendance;
 import com.wemo.backend.domain.plan.entity.Plan;
 import com.wemo.backend.domain.user.entity.User;
+
+import java.util.List;
 
 public interface PlanReader {
 
     Plan getPlan(Long planId);
 
     void validateAttendance(User user, Plan plan);
+
+    List<Plan> getPlanByMeeting(Meeting meeting);
+
+    List<Attendance> getAttendanceList(Plan plan);
 
 }
