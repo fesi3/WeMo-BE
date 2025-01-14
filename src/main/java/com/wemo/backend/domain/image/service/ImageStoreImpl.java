57ace7a5fa7fff6e914c95f897c73f37e6907bba
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-14 17:35:00 +0900

    [Refactor] 이미지 저장 시 대표 이미지 설정

--- src/main/java/com/wemo/backend/domain/image/service/ImageStoreImpl.java
+++ src/main/java/com/wemo/backend/domain/image/service/ImageStoreImpl.java
@@ -3,8 +3,12 @@ package com.wemo.backend.domain.image.service;
 import com.wemo.backend.domain.image.entity.Image;
 import com.wemo.backend.domain.image.repository.ImageRepository;
 import com.wemo.backend.domain.user.entity.User;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -13,6 +17,7 @@ public class ImageStoreImpl implements ImageStore {
     private final ImageRepository imageRepository;
 
     @Override
+    @Transactional
     public Image storeImage(User user, Long entityId, String fileUrl, Image.EntityType entityType) {
 
         Image image = Image.builder()
@@ -25,6 +30,37 @@ public class ImageStoreImpl implements ImageStore {
         imageRepository.save(image);
 
         return image;
+    }
+
+    @Override
+    @Transactional
+    public List<String> storeImageList(User user, Long entityId, List<String> fileUrls, Image.EntityType entityType) {
+        List<String> imageList = new ArrayList<>();
+
+        for (int i = 0; i < fileUrls.size(); i++) {
+            String fileUrl = fileUrls.get(i);
+
+            // 이미지 생성
+            Image image = Image.builder()
+                    .user(user)
+                    .entityType(entityType)
+                    .entityId(entityId)
+                    .fileUrl(fileUrl)
+                    .build();
+
+            // 첫 번째 이미지라면 대표 이미지로 설정
+            if (i == 0) {
+                image.updateMain(); // 첫 번째 이미지는 대표 이미지로 설정
+            }
+
+            // 이미지 저장
+            imageRepository.save(image);
+
+            // 저장된 이미지 URL을 리스트에 추가
+            imageList.add(image.getFileUrl());
+        }
+
+        return imageList;
     }
 
 }
