cc5934b12a435ba2ee46290c2912c60e05d227e4
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-15 03:00:15 +0900

    [Fix] Like 예약어 충돌로 Likes로 변경

--- src/main/java/com/wemo/backend/domain/like/repository/LikeRepository.java
+++ src/main/java/com/wemo/backend/domain/like/repository/LikeRepository.java
@@ -1,15 +1,15 @@
 package com.wemo.backend.domain.like.repository;
 
-import com.wemo.backend.domain.like.entity.Like;
+import com.wemo.backend.domain.like.entity.Likes;
 import com.wemo.backend.domain.plan.entity.Plan;
 import com.wemo.backend.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LikeRepository extends JpaRepository<Like, Long> {
+public interface LikeRepository extends JpaRepository<Likes, Long> {
 
     boolean existsByUserAndPlan(User user, Plan plan);
 
-    Like findByUserAndPlan(User user, Plan plan);
+    Likes findByUserAndPlan(User user, Plan plan);
 
     int countByPlan(Plan plan);
 
