7cc6821c3a204b9848ab367444cc832c99f36195
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-15 07:08:17 +0900

    [Feat] 유저가 좋아요한 일정 조회 기능 구현

--- src/main/java/com/wemo/backend/domain/like/service/LikeReader.java
+++ src/main/java/com/wemo/backend/domain/like/service/LikeReader.java
@@ -1,12 +1,17 @@
 package com.wemo.backend.domain.like.service;
 
+import com.wemo.backend.domain.like.entity.Likes;
 import com.wemo.backend.domain.plan.entity.Plan;
 import com.wemo.backend.domain.user.entity.User;
+
+import java.util.List;
 
 public interface LikeReader {
 
     void validateLike(User user, Plan plan);
 
     void validateLikeToDelete(User user, Plan plan);
+
+    List<Likes> getLikeCountByUser(User user);
 
 }
--- src/main/java/com/wemo/backend/domain/like/service/LikeReaderImpl.java
+++ src/main/java/com/wemo/backend/domain/like/service/LikeReaderImpl.java
@@ -1,11 +1,14 @@
 package com.wemo.backend.domain.like.service;
 
+import com.wemo.backend.domain.like.entity.Likes;
 import com.wemo.backend.domain.like.repository.LikeRepository;
 import com.wemo.backend.domain.plan.entity.Plan;
 import com.wemo.backend.domain.user.entity.User;
 import com.wemo.backend.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 import static com.wemo.backend.global.exception.ErrorCode.*;
 
@@ -26,6 +29,12 @@ public class LikeReaderImpl implements LikeReader{
     public void validateLikeToDelete(User user, Plan plan) {
 
         if (!likeRepository.existsByUserAndPlan(user, plan)) throw new CustomException(LIKE_NOT_FOUND);
+    }
+
+    @Override
+    public List<Likes> getLikeCountByUser(User user) {
+
+        return likeRepository.findAllByUser(user);
     }
 
 }
