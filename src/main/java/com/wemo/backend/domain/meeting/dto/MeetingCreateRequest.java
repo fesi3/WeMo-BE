7ee142659179908cdb2e92e8c379c13b787eb7e3
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-19 22:50:08 +0900

    [Fix] 모임 생성 시 이미지 값 조건 수정 (@NotBlank -> @NotEmpty)

--- src/main/java/com/wemo/backend/domain/meeting/dto/MeetingCreateRequest.java
+++ src/main/java/com/wemo/backend/domain/meeting/dto/MeetingCreateRequest.java
@@ -1,9 +1,6 @@
 package com.wemo.backend.domain.meeting.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -25,7 +22,7 @@ public class MeetingCreateRequest {
     @PositiveOrZero(message = "카테고리 id는 양수 값이어야 합니다.")
     private Long categoryId;
 
-    @NotBlank(message = "모임 대표 이미지는 필수 입력 값입니다.")
+    @NotEmpty(message = "모임 대표 이미지는 필수 입력 값입니다.")
     private List<String> fileUrls;
 
 }
