9227ec30ae238dd67112711de00dd9c2c0951f79
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-01-25 20:26:42 +0900

    [Refactor] 회원 가입 및 로그인 시 데이터 유효성 검사 추가

--- src/main/java/com/wemo/backend/domain/user/dto/SigninRequest.java
+++ src/main/java/com/wemo/backend/domain/user/dto/SigninRequest.java
@@ -11,6 +11,7 @@ public class SigninRequest {
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
 
 }
