8473b5971e928cf93c85182ee7355de6bd3ffa69
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-02-07 00:19:04 +0900

    [Style] 필터 도메인 확인 로그 제거

--- src/main/java/com/wemo/backend/domain/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/wemo/backend/domain/auth/JwtAuthenticationFilter.java
@@ -140,7 +140,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private void logRequestInfo(HttpServletRequest request) {
 
-        log.info("현재 요청된 도메인: {}", request.getServerName());
         log.info("요청 IP: {}, 요청 URI: {}", request.getRemoteAddr(), request.getRequestURI());
     }
 
