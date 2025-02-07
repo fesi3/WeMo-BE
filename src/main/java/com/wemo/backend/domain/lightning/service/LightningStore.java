0e0f1bf92127b48fa79d2951bd9a51b72034d5e3
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-02-07 12:47:17 +0900

    [Feat] 번개 모임에 지역 데이터 추가 저장

--- src/main/java/com/wemo/backend/domain/lightning/service/LightningStore.java
+++ src/main/java/com/wemo/backend/domain/lightning/service/LightningStore.java
@@ -3,10 +3,11 @@ package com.wemo.backend.domain.lightning.service;
 import com.wemo.backend.domain.lightning.dto.LightningCreateRequest;
 import com.wemo.backend.domain.lightning.entity.Lightning;
 import com.wemo.backend.domain.lightning.entity.LightningType;
+import com.wemo.backend.domain.region.entity.District;
 import com.wemo.backend.domain.user.entity.User;
 
 public interface LightningStore {
 
-    Lightning store(User user, LightningType lightningType, LightningCreateRequest request);
+    Lightning store(User user, LightningType lightningType, District district, LightningCreateRequest request);
 
 }
