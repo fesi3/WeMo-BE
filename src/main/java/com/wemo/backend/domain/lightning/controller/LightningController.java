b98a25acf05aa56b5dd5c5feb7f8cfa88e21957d
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-02-07 12:48:46 +0900

    [Feat] 번개 모임 목록 조회 기능 구현

--- src/main/java/com/wemo/backend/domain/lightning/controller/LightningController.java
+++ src/main/java/com/wemo/backend/domain/lightning/controller/LightningController.java
@@ -3,21 +3,21 @@ package com.wemo.backend.domain.lightning.controller;
 import com.wemo.backend.domain.auth.UserDetailsImpl;
 import com.wemo.backend.domain.lightning.dto.LightningCreateRequest;
 import com.wemo.backend.domain.lightning.dto.LightningCreateResponse;
+import com.wemo.backend.domain.lightning.dto.LightningCursorPagingResponse;
 import com.wemo.backend.domain.lightning.service.LightningService;
 import com.wemo.backend.global.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Lightnings")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/lightnings")
@@ -36,6 +36,23 @@ public class LightningController {
                                                                                      @Valid @RequestBody LightningCreateRequest request) {
 
         return ResponseEntity.status(201).body(SuccessResponse.successWithData(lightningService.createLightnings(userDetails.getUsername(), request)));
+    }
+
+    @Operation(summary = "번개 모임 목록 조회", description = "번개 모임 일정의 목록을 반환합니다.")
+    @ApiResponse(responseCode = "200", description = "요청한 일정 목록이 반환되었습니다.")
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<SuccessResponse<LightningCursorPagingResponse>> getLightningMeetingList(@RequestParam(required = false) Long cursor,
+                                                                                                  @RequestParam(required = false, defaultValue = "10") int size,
+                                                                                                  @RequestParam(required = false) String province,
+                                                                                                  @RequestParam(required = false) String district,
+                                                                                                  @RequestParam(required = false) Long lightningTypeId,
+                                                                                                  @RequestParam(required = false) Integer lightningTimeId,
+                                                                                                  @RequestParam(required = false) Double latitude,
+                                                                                                  @RequestParam(required = false) Double longitude,
+                                                                                                  @RequestParam(required = false, defaultValue = "1.0") Double radius) {
+
+        return ResponseEntity.ok(SuccessResponse.successWithData(lightningService.getLightningMeetingList(cursor, size, province, district,
+                lightningTypeId, lightningTimeId, latitude, longitude, radius)));
     }
 
 }
---
+++ src/main/java/com/wemo/backend/domain/lightning/repository/querydsl/LightningCursorQueryDslImpl.java
@@ -0,0 +1,137 @@
+package com.wemo.backend.domain.lightning.repository.querydsl;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.wemo.backend.domain.lightning.dto.LightningCursorPagingResponse;
+import com.wemo.backend.domain.lightning.dto.LightningListResponse;
+import com.wemo.backend.domain.lightning.entity.DateType;
+import com.wemo.backend.domain.region.entity.QDistrict;
+import jakarta.persistence.EntityManager;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+import static com.wemo.backend.domain.lightning.entity.QLightning.lightning;
+import static com.wemo.backend.domain.lightningJoin.entity.QLightningJoin.lightningJoin;
+import static com.wemo.backend.domain.user.entity.QUser.user;
+
+@Slf4j
+public class LightningCursorQueryDslImpl implements LightningCursorQueryDsl {
+
+    private final JPAQueryFactory queryFactory;
+
+    public LightningCursorQueryDslImpl(EntityManager em) {
+
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public LightningCursorPagingResponse getLightningMeetingList(Long cursor, int size, String province, String district, Long lightningTypeId, Integer lightningTimeId, Double latitude, Double longitude, Double radius) {
+
+        log.info("번개 모임 목록 조회 요청");
+
+        // 커서 조건을 제외한 기본 조건 빌더
+        BooleanBuilder baseConditions = new BooleanBuilder()
+                .and(buildFilterConditions(province, district, lightningTypeId, lightningTimeId, latitude, longitude, radius));
+
+        // 목록 조회 쿼리 (커서 조건 포함)
+        BooleanBuilder listConditions = new BooleanBuilder(baseConditions);
+        if (cursor != null) {
+            listConditions.and(lightning.id.lt(cursor));
+        }
+
+        List<LightningListResponse> lightningListResponses = queryFactory
+                .select(
+                        Projections.constructor(
+                                LightningListResponse.class,
+                                lightning.id,
+                                lightning.lightningName,
+                                lightning.lightningType.lightTypeName,
+                                lightning.dateType.stringValue(),
+                                lightning.lightningDate,
+                                lightning.lightningCapacity,
+                                Expressions.as(
+                                        JPAExpressions.select(lightningJoin.count())
+                                                .from(lightningJoin)
+                                                .where(lightningJoin.lightning.id.eq(lightning.id)),
+                                        "lightningParticipants"
+                                ),
+                                lightning.address,
+                                lightning.latitude,
+                                lightning.longitude,
+                                lightning.createdAt,
+                                lightning.updatedAt,
+                                user.nickname,
+                                user.profileImagePath
+                        )
+                )
+                .from(lightning)
+                .leftJoin(lightning.user, user)
+                .where(listConditions)
+                .limit(size)
+                .orderBy(lightning.createdAt.desc())
+                .fetch();
+
+        // 쿼리 결과 후, lightningTime 값에 대해 getDisplayName()을 호출하여 변환
+        lightningListResponses.forEach(response -> {
+            response.setLightningTime(DateType.valueOf(response.getLightningTime()).getDisplayName());  // 변환된 값으로 설정
+        });
+
+        return new LightningCursorPagingResponse(lightningListResponses, lightningListResponses.size());
+    }
+
+    private BooleanExpression buildFilterConditions(String province, String district, Long lightningTypeId, Integer lightningTimeId, Double latitude, Double longitude, Double radius) {
+
+        BooleanExpression condition = lightning.deletedAt.isNull(); // 기본 필터 조건
+
+        condition = buildRegionFilter(province, district, condition, lightning.district);
+
+        if (lightningTypeId != null) {
+            condition = condition.and(lightning.lightningType.lightningTypeId.eq(lightningTypeId));
+        }
+
+        if (lightningTimeId != null && (lightningTimeId == 1 || lightningTimeId == 2 || lightningTimeId == 3)) {
+
+            DateType dateType = DateType.fromId(lightningTimeId);
+            condition = condition.and(lightning.dateType.eq(dateType));
+        }
+
+        return buildAddressFilter(province, district, latitude, longitude, radius, condition);
+    }
+
+    public static BooleanExpression buildRegionFilter(String province, String district, BooleanExpression condition, QDistrict district2) {
+
+        if (province != null && !province.isEmpty()) {
+            condition = condition.and(district2.province.provinceName.eq(province));
+        }
+
+        if (district != null && !district.isEmpty()) {
+            condition = condition.and(district2.districtName.eq(district));
+        }
+        return condition;
+    }
+
+
+    public static BooleanExpression buildAddressFilter(String province, String district, Double latitude, Double longitude, Double radius, BooleanExpression condition) {
+
+        if ((province == null || province.isEmpty()) && (district == null || district.isEmpty())) {
+            if (latitude != null && longitude != null && radius != null) {
+                double radiusInMeters = radius * 1000; // km → m 변환
+
+                NumberExpression<Double> distance = Expressions.numberTemplate(Double.class,
+                        "ST_Distance_Sphere(point({0}, {1}), point(plan.longitude, plan.latitude))",
+                        longitude, latitude);
+
+                condition = condition.and(distance.loe(radiusInMeters));
+            }
+        }
+
+        return condition;
+    }
+
+}
