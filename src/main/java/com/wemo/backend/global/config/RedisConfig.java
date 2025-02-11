13253c3108cc02ecb7106ae1b117a14a2306c446
ohyeryung <dpfu1948@g.eulji.ac.kr>
2025-02-11 19:55:32 +0900

    [Refactor] RedisConfig 비밀번호 설정 추가

--- src/main/java/com/wemo/backend/global/config/RedisConfig.java
+++ src/main/java/com/wemo/backend/global/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -17,17 +18,25 @@ public class RedisConfig {
     @Value("${REDIS_PORT}")
     private int redisPort;
 
+    @Value("${REDIS_PASSWORD}")
+    private String redisPassword;  // 비밀번호 추가
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        config.setPassword(redisPassword);  // 비밀번호 설정
         return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;
     }
+
 }
